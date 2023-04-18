798f0e5e8b50f518feaae68a43ee8c5851211ea0
Rodrigooou <rm550816@fiap.com.br>
2023-04-18 10:49:39 -0300

    COMMITADO manipulando elementos da lista

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -1,5 +1,3 @@
-const liElement = document.getElementsByTagName("li");
-
     
 
 // for (let index = 0; index < liElement.length; index++) {
@@ -13,16 +11,33 @@ const liElement = document.getElementsByTagName("li");
 //Declaração de Arrays
 let nr1 = [1,2,3,4,5];
 let nr2 = [6,7,8,9,10];
-let nr3 = [nr1+nr2];
+//Espalhar o array com SPREAD [...]
+let nr3 = [...nr1,...nr2];
 
 console.log(`Array-1 ${nr1}`);
 console.log(`Array-2 ${nr2}`);
 console.log(`Array-3 ${nr3}`);
 
-
 //Listando um Array tecnica especial
-nr3.forEach((ItemDoArray)=> {
+// nr3.forEach((ItemDoArray)=> {
+//     console.log(`Array-3 sendo listada ${ItemDoArray}`);
+// })
 
 
-console.log(`Array-3 sendo listada ${nr3}`);
+//Listando o Array com o MAP
+//numero e key são elementos
+nr3.map((numero,key)=>{
+    
+    console.log(`${(key+1)} - Itens do array - ${numero}`);
+    
+    
+})
+
+//Convertendo HTMLCollection em Array de forma indireta
+// const liElement = document.getElementsByTagName("li");
+const liElementHtml = document.getElementsByTagName("li");
+const liElementArray = [...liElementHtml];
+
+liElementArray.map((item, key)=>{
+    console.log(`${key+1} -I${item.textContent}`)
 })
