8269eb3144a882f87f45e2e4756b2806cbd1015c
Rodrigooou <rm550816@fiap.com.br>
2023-05-02 10:38:06 -0300

    NO meio da aula

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -81,7 +81,7 @@ function mudaCor(){
     const cabecalho = document.querySelector(".cabecalho");
     cabecalho.setAttribute("style", `background-color:rgb(${r},${g},${b})`)
 
-    tmp = setTimeout(mudaCor, 500)
+    tmp = setTimeout(mudaCor, 1000)
 
 }
 mudaCor();
@@ -90,28 +90,34 @@ mudaCor();
 // Função Altera Banner
 let temp
 function alteraBanner(){
-    let nr = Math.round(Math.random()*3);
+    let nr = Math.ceil(Math.random()*3);
     
     let caminho = `./img/banner-lateral-${nr}.png`;
 
     const img1 = document.querySelector(".l-e > img");
-    img1.src = caminho;
+    const img2 = document.querySelector(".l-d > img");
     
+    img1.src = caminho;
+    img2.src = caminho;
     temp = setTimeout(alteraBanner, 1000)
 }
 alteraBanner();
 
-let tempo
-function alteraBannerr(){
-    let num = Math.round(Math.random()*3);
+//Para fazer os banners mudarem um diferente do outro.
+
+// let tempo
+// function alteraBannerr(){
+//     let num = Math.ceil(Math.random()*3);
     
-    let caminhoo = `./img/banner-lateral-${num}.png`;
+//     let caminhoo = `./img/banner-lateral-${num}.png`;
    
-    const imagem = document.querySelector(".l-d > img");
-    imagem.src = caminhoo;
+//     const imagem = document.querySelector(".l-d > img");
+//     imagem.src = caminhoo;
     
-    tempo = setTimeout(alteraBannerr, 1000)
-}
-alteraBannerr()
+//     tempo = setTimeout(alteraBannerr, 1000)
+// }
+// alteraBannerr()
+
+
 
 
